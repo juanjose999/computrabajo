ed3f21134e73d1afe681619024d4f4f4180b9791
juanjose999 <juanjose000012@gmail.com>
2024-10-10 07:32:03 -0500

    first commit

---
+++ src/main/java/com/computrabajo/ComputrabajoApplication.java
@@ -0,0 +1,13 @@
+package com.computrabajo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputrabajoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputrabajoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/computrabajo/ComputrabajoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.computrabajo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputrabajoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
